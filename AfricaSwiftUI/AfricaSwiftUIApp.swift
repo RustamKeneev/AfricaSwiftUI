db29b24233532ecea3048a7a926d253b79c4f93b
rustamkeneev <dr.rustamkeneev@gmail.com>
2021-07-19 00:39:24 +0600

    Initial Commit

---
+++ AfricaSwiftUI/AfricaSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaSwiftUIApp.swift
+//  AfricaSwiftUI
+//
+//  Created by Rustam Keneev on 7/19/21.
+//
+
+import SwiftUI
+
+@main
+struct AfricaSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
