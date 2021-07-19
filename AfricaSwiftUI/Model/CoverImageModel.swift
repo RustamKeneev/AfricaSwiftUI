20f432b0c975aacf14ed9a7bd9cab8f2242cda9f
rustamkeneev <dr.rustamkeneev@gmail.com>
2021-07-20 00:15:20 +0600

    added CoverImageModel

---
+++ AfricaSwiftUI/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  AfricaSwiftUI
+//
+//  Created by Rustam Keneev on 7/20/21.
+//
+
+import Foundation
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
