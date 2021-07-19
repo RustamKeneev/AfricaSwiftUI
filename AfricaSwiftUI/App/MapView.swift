caa34ffa369ad8c9376e1c614af0487d2e75f88c
rustamkeneev <dr.rustamkeneev@gmail.com>
2021-07-19 23:51:26 +0600

    added views (GalleryView, MapView, VideoListView, MainView) and start added Tabview

---
+++ AfricaSwiftUI/App/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  AfricaSwiftUI
+//
+//  Created by Rustam Keneev on 7/19/21.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("Map")
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
---
+++ AfricaSwiftUI/App/VideoListView.swift
@@ -0,0 +1,20 @@
+//
+//  VideoListView.swift
+//  AfricaSwiftUI
+//
+//  Created by Rustam Keneev on 7/19/21.
+//
+
+import SwiftUI
+
+struct VideoListView: View {
+    var body: some View {
+        Text("Video")
+    }
+}
+
+struct VideoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoListView()
+    }
+}
